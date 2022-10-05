6fb25fdbc4f2fa00e0f1dbfb9c34322004a69e39
aniyuksh <ozaaniyuksh@gmail.com>
2022-10-05 09:30:15 +0530

    done styling and completed the code

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 var dateOfBirth = document.querySelector("#date-of-birth");
 var luckyNumber = document.querySelector("#lucky-number");
 var checkButton = document.querySelector("#check-number");
+var output = document.querySelector("#output");
+
+function compareValues(sum , luckyNumber){
+ if(sum % luckyNumber === 0){
+  output.value = "YAY, your bday is lucky";
+ }
+ else{
+  output.value = "OOPS, your bday is not lucky.!";
+ }
+}
 
 function checkBirthdayLucky(){
   var dob = dateOfBirth.value;
   var sum = calculateSum(dob);
-  console.log(sum); 
+  if(sum && dob){
+  compareValues(sum, luckyNumber.value);
+  }
+  else{
+    alert("Enter both the fields");
+    output.value = "Enter both the fields";
+  }
 }
 
 function calculateSum(dob){
