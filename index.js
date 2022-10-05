88986591d8f8646332ff7a891710a6d6f3f16224
aniyuksh <ozaaniyuksh@gmail.com>
2022-10-05 08:57:45 +0530

    done with the calculate sum of DOB

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
-var dob = document.querySelector("#date-of-birth");
+var dateOfBirth = document.querySelector("#date-of-birth");
 var luckyNumber = document.querySelector("#lucky-number");
 var checkButton = document.querySelector("#check-number");
 
+function checkBirthdayLucky(){
+  var dob = dateOfBirth.value;
+  var sum = calculateSum(dob);
+  console.log(sum); 
+}
 
-checkButton.addEventListener("click", function getvalues(){
-  console.log(dob.value, luckyNumber.value);
-})
+function calculateSum(dob){
+  dob = dob.replaceAll("-","");
+  var sum = 0;
+  for(var i = 0 ; i < dob.length;i++ ){
+    sum = sum + Number(dob.charAt(i));
+  }
+  return sum;
+}
+
+checkButton.addEventListener("click",checkBirthdayLucky)
